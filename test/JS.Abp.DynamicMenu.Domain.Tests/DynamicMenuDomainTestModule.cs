374042c73e7dd731c75507baf63282ae2c3ff0cd
zhaofenglee <zhaof_li@163.com>
2024-04-17 21:29:20 +0800

    Add unit testing.

--- test/JS.Abp.DynamicMenu.Domain.Tests/DynamicMenuDomainTestModule.cs
+++ test/JS.Abp.DynamicMenu.Domain.Tests/DynamicMenuDomainTestModule.cs
@@ -1,15 +1,11 @@
-﻿using JS.Abp.DynamicMenu.EntityFrameworkCore;
-using Volo.Abp.Modularity;
+﻿using Volo.Abp.Modularity;
 
 namespace JS.Abp.DynamicMenu;
 
-/* Domain tests are configured to use the EF Core provider.
- * You can switch to MongoDB, however your domain tests should be
- * database independent anyway.
- */
 [DependsOn(
-    typeof(DynamicMenuEntityFrameworkCoreTestModule)
-    )]
+    typeof(DynamicMenuDomainModule),
+    typeof(DynamicMenuTestBaseModule)
+)]
 public class DynamicMenuDomainTestModule : AbpModule
 {
 
---
+++ test/JS.Abp.DynamicMenu.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/MenuItems/EfCoreMenuItemsAppServiceTests.cs
@@ -0,0 +1,8 @@
+using JS.Abp.DynamicMenu.MenuItems;
+using Xunit;
+
+namespace JS.Abp.DynamicMenu.EntityFrameworkCore.Applications.MenuItems;
+
+public class EfCoreMenuItemsAppServiceTests : MenuItemsAppServiceTests<DynamicMenuEntityFrameworkCoreTestModule>
+{
+}
