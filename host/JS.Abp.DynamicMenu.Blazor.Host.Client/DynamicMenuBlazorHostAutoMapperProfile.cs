9b48878e62ae0fc1ca035e10a350df4f65dc1c5b
zhaofenglee <zhaof_li@163.com>
2024-05-26 09:19:07 +0800

    Upgrade Abp to 8.2.0-rc.3.

---
+++ host/JS.Abp.DynamicMenu.Blazor.Host.Client/DynamicMenuBlazorHostAutoMapperProfile.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+
+namespace JS.Abp.DynamicMenu.Blazor.Host.Client;
+
+public class DynamicMenuBlazorHostAutoMapperProfile : Profile
+{
+    public DynamicMenuBlazorHostAutoMapperProfile()
+    {
+        //Define your AutoMapper configuration here for the Blazor project.
+    }
+}
