93ed2b994249437dc9f4454e2005f09b13c8855e
zhaofenglee <zhaof_li@163.com>
2024-11-22 19:28:33 +0800

    Upgrade Abp to 9.0.0.

--- host/JS.Abp.DynamicMenu.Web.Host/DynamicMenuWebHostModule.cs
+++ host/JS.Abp.DynamicMenu.Web.Host/DynamicMenuWebHostModule.cs
@@ -231,7 +231,7 @@ public class DynamicMenuWebHostModule : AbpModule
         }
 
         app.UseHttpsRedirection();
-        app.UseStaticFiles();
+        app.MapAbpStaticAssets();
         app.UseRouting();
         app.UseAuthentication();
 
