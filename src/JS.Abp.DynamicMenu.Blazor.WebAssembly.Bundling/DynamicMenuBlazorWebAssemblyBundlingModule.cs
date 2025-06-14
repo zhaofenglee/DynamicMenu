95350c989924c6f095692d00508561f69f97fb9a
zhaofenglee <zhaof_li@163.com>
2025-06-14 17:51:06 +0800

    Add DynamicMenu bundling module and contributors for Blazor WebAssembly

---
+++ src/JS.Abp.DynamicMenu.Blazor.WebAssembly.Bundling/DynamicMenuBlazorWebAssemblyBundlingModule.cs
@@ -0,0 +1,23 @@
+﻿using Volo.Abp.AspNetCore.Components.WebAssembly.Theming.Bundling;
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+using Volo.Abp.Modularity;
+
+namespace JS.Abp.DynamicMenu.Blazor.WebAssembly.Bundling;
+
+[DependsOn(
+    typeof(AbpAspNetCoreComponentsWebAssemblyThemingBundlingModule)
+)]
+public class DynamicMenuBlazorWebAssemblyBundlingModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        Configure<AbpBundlingOptions>(options =>
+        {
+            var globalStyles = options.StyleBundles.Get(BlazorWebAssemblyStandardBundles.Styles.Global);
+            globalStyles.AddContributors(typeof(DynamicMenuBundleStyleContributor));
+
+            var globalScripts = options.ScriptBundles.Get(BlazorWebAssemblyStandardBundles.Scripts.Global);
+            globalScripts.AddContributors(typeof(DynamicMenuBundleScriptContributor));
+        });
+    }
+}
---
+++ src/JS.Abp.DynamicMenu.Blazor.WebAssembly.Bundling/DynamicMenuBundleScriptContributor.cs
@@ -0,0 +1,11 @@
+using Volo.Abp.AspNetCore.Mvc.UI.Bundling;
+
+namespace JS.Abp.DynamicMenu.Blazor.WebAssembly.Bundling;
+
+public class DynamicMenuBundleStyleContributor : BundleContributor
+{
+    public override void ConfigureBundle(BundleConfigurationContext context)
+    {
+        // Add your css files here
+    }
+}
